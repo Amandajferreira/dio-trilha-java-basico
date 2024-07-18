01f478713112c1c21be6d8b957684454dd90af3d
Amanda Ferreira <53319956+Amandajferreira@users.noreply.github.com>
2024-07-18 20:44:13 -0300

    Terminal

---
+++ java-terminal/calculadora/src/MinhaClasse.java
@@ -0,0 +1,10 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class MinhaClasse {
+   public MinhaClasse() {
+   }
+
+   public static void main(String[] args) {
+      System.out.println("Oi turma, fui executado pelo Terminal");
+   }
+
+}
